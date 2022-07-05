9f7d41421336a2653ce9213ca0d1f9abd8839e6f
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-07-05 17:09:24 -0600

    Add gems

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,7 +8,7 @@ class Invoice < ApplicationRecord
 
   validates :status, presence: true
 
-  enum status: {'cancelled' => 0, 'in progress' => 1, 'completed' => 2}
+  enum status: { 'cancelled' => 0, 'in progress' => 1, 'completed' => 2 }
 
   def invoice_customer
     "#{customer.first_name} #{customer.last_name}"
@@ -16,9 +16,9 @@ class Invoice < ApplicationRecord
 
   def self.incomplete_invoices
     joins(:invoice_items)
-    .where.not(invoice_items: {status: 1})
-    .distinct
-    .order(:created_at)
+      .where.not(invoice_items: { status: 1 })
+      .distinct
+      .order(:created_at)
   end
 
   def self.invoices_with_merchant_items(merchant)
@@ -31,12 +31,12 @@ class Invoice < ApplicationRecord
 
   def discounted_revenue
     discount =
-    invoice_items
-    .joins(:discounts)
-    .where('invoice_items.quantity >= discounts.quantity_threshold')
-    .select('invoice_items.id, max((discounts.percentage) * (invoice_items.quantity * invoice_items.unit_price)) AS discounted') # SUM(percent_discount * (quantity * price))/100
-    .group('invoice_items.id')
-    .sum(&:discounted)/100
+      invoice_items
+      .joins(:discounts)
+      .where('invoice_items.quantity >= discounts.quantity_threshold')
+      .select('invoice_items.id, max((discounts.percentage) * (invoice_items.quantity * invoice_items.unit_price)) AS discounted') # SUM(percent_discount * (quantity * price))/100
+      .group('invoice_items.id')
+      .sum(&:discounted)/100
 
     total_revenue - discount
   end
@@ -44,4 +44,4 @@ end
 
 
 # put in invoice_items model
- # sum discounted
+# sum discounted
